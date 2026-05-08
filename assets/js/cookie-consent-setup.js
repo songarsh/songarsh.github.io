80120ed73a1ac1cfcc5bf9fa9b64e3938acf510f
songarsh <s.harsh33.hs@gmail.com>
2026-05-08 22:57:40 +0000

    Deploying to gh-pages from @ songarsh/songarsh.github.io@1c1f2bb2d59588b15b98ce46068842ea91ce8f39 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://hsongara.com/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://songarsh.github.io/#contact">contact us</a>.'
               }
             ]
           }
